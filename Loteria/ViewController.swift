7617a05fcd39e488393149f9a386302c5005afa8
Rodrigo Figueiredo <rmfmaster@gmail.com>
2019-05-16 10:58:03 -0300

    Initial Commit

---
+++ Loteria/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Loteria
+//
+//  Created by Rodrigo Figueiredo on 16/05/19.
+//  Copyright © 2019 Rodrigo Figueiredo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
